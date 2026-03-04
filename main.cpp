2b74f3164116dd1575b6ce1499d84a638eb4e25b
Jhony279 <lordjhony732@gmail.comm>
2026-03-04 01:52:26 -0800

    Ending coding session

--- main.cpp
+++ main.cpp
@@ -40,69 +40,6 @@ int main() {
     }
     output(head);
 
-    // // deleting a node
-    // Node * current = head;
-    // cout << "Which node to delete? " << endl;
-    // output(head);
-    // int entry;
-    // cout << "Choice --> ";
-    // cin >> entry;
-
-    // // traverse that many times and delete that node
-    // current = head;
-    // Node *prev = head;
-    // for (int i = 0; i < (entry-1); i++)
-    //     if (i == 0)
-    //         current = current->next;
-    //     else {
-    //         current = current->next;
-    //         prev = prev->next;
-    //     }
-    // // at this point, delete current and reroute pointers
-    // if (current) {  // checks for current to be valid before deleting the node
-    //     prev->next = current->next;
-    //     delete current;
-    //     current = nullptr;
-    // }
-    // output(head);
-
-    // // insert a node
-    // current = head;
-    // cout << "After which node to insert 10000? " << endl;
-    // count = 1;
-    // while (current) {
-    //     cout << "[" << count++ << "] " << current->value << endl;
-    //     current = current->next;
-    // }
-    // cout << "Choice --> ";
-    // cin >> entry;
-
-    // current = head;
-    // prev = head;
-    // for (int i = 0; i < (entry); i++)
-    //     if (i == 0)
-    //         current = current->next;
-    //     else {
-    //         current = current->next;
-    //         prev = prev->next;
-    //     }
-    // //at this point, insert a node between prev and current
-    // Node * newnode = new Node;
-    // newnode->value = 10000;
-    // newnode->next = current;
-    // prev->next = newnode;
-    // output(head);
-
-    // // deleting the linked list
-    // current = head;
-    // while (current) {
-    //     head = current->next;
-    //     delete current;
-    //     current = head;
-    // }
-    // head = nullptr;
-    // output(head);
-
     deleteNode(head);
     insertNode(head);
     deleteList(head);
